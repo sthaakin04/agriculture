c3ff468d09866f4eaafb1f18db73d2888ca3b685
sthaakin04 <akinstha97@gmail.com>
2022-12-22 12:12:51 +0545

    created project and added home and nav

---
+++ src/Components/HomeContents/HomeContents.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import "./HomeContents.css"
+
+export default function HomeContents() {
+  return (
+    <div>
+        <div className="container">
+  <h1>Mountain Star Zlatibor</h1>
+  <p>Zlatibor is a mountain of exceptional beauty whose special geographical properties have made this mountain a real gem of western Serbia.</p>
+  <a href="#">Learn more</a>
+</div>
+
+<div className="blank">
+    <h2>Hey we are an agricultural company we help u to grow the best goodies</h2>
+</div>
+
+<div className="container second">
+  <div className="item">
+    <div className="img img-first"></div>
+    <div className="card">
+      <h3>Rock climbing</h3>
+      <p>The goal is to reach the summit of a formation or the endpoint of a usually pre-defined route without falling</p>
+      <a href="#">Learn more</a>
+    </div>
+  </div>
+  <div className="item">
+    <div className="img img-second"></div>
+    <div className="card">
+      <h3>Caving</h3>
+      <p>Exploring underground through networks of tunnels and passageways, which can be natural or artificial.</p>
+      <a href="#">Learn more</a>
+    </div>
+  </div>
+  <div className="item">
+    <div className="img img-third"></div>
+    <div className="card">
+      <h3>Parachuting</h3>
+      <p>Jumping from an aeroplane and falling through the air before opening your parachute.</p>
+      <a href="#">Learn more</a>
+    </div>
+  </div>
+</div>
+
+<div className="blank"></div>
+    </div>
+  )
+}
