1b7c5b7b8b2949b2e4dce927b6d9f7187df2cbe1
sthaakin04 <akinstha97@gmail.com>
2022-12-23 09:57:41 +0545

    added footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <div>
+
+    <div id="footer2">
+      <footer>
+        <ul class="navCat">
+          <li class="topLevel">About</li>
+          <li>About Us</li>
+          <li>Our Story</li>
+          <li>FAQ</li>
+        </ul>
+        <ul class="navCat">
+          <li class="topLevel">Services</li>
+          <li>Custom Orders</li>
+          <li>Wholesale</li>
+        </ul>
+        <ul class="navCat">
+          <li class="topLevel">Contact</li>
+          <li>Customer Service</li>
+          <li>Careers</li>
+          <li>Community</li>
+          <li>Social Media</li>
+        </ul>
+      </footer>
+    </div>
+    </div>
+  )
+}
