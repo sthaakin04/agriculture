01d8d1162ff8b65beb24c06c09f9a0b37c55bdc6
sthaakin04 <akinstha97@gmail.com>
2022-12-23 12:20:50 +0545

    home contents

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,6 +10,23 @@ export default function Home() {
         <h2>Grow on your own</h2>
         <Link to="/about" className="boxBtn">More about us</Link>
       </section>
+
+      <div class="about-us">
+      <div class="text">
+          <h2>Discover</h2>
+          <h3>Our Story</h3>
+          <div><i class="fas fa-asterisk"></i></div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolores sit ipsam incidunt soluta aspernatur itaque pariatur! Fuga id provident quas vitae, dolore error mollitia excepturi iusto repellat, blanditiis deleniti.</p>
+      </div>
+      <div class="image-container">
+          <div class="image image1">
+              <img src="https://res.cloudinary.com/abdel-rahman-ali/image/upload/v1535988527/vertical-photo-1.jpg" alt="Food Photo"/>
+          </div>
+          <div class="image image2">
+              <img src="https://res.cloudinary.com/abdel-rahman-ali/image/upload/v1535988532/vertical-photo-2.jpg" alt="Food Photo"/>
+          </div>
+      </div>
+  </div>
     </div>
   )
 }
