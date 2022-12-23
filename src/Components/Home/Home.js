55ab37787f6558830731ea4b31d19a13a16285dd
bipul077 <bipulratna07@gmail.com>
2022-12-23 10:44:03 +0545

    header changed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import HomeContents from '../HomeContents/HomeContents'
-
+import './Home.css'
 export default function Home() {
   return (
     <div>
-      <HomeContents/>
+      <section class="box">
+        <video src="static/images/fieldvideo.mp4" autoPlay muted Loop></video>
+        <h1>Yoga Center</h1>
+        <h1>Find your own path in yoga</h1>
+        <a href="#" class="boxBtn">Contact Us</a>
+    </section>
+      {/* <HomeContents/> */}
     </div>
   )
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,30 +2,17 @@ import React from 'react'
 import './Navbar.css'
 
 export default function Navbar() {
-    const menu=(e) => {
-        let menu = document.querySelector('#menu-icon');
-        let navbar = document.querySelector('.navbar');
-        
-        menu.onclick = () => {
-          menu.classList.toggle('bx-x');
-          navbar.classList.toggle('open');
-        }
-      }
   return (
     <div>
         <header>
-            {/* <Link to='#' className='logo'><img src="static/images/logo.png"/><span>Logo</span></Link> */}
-            <a href='/' className='logo'><img src="static/images/logotest.png"/></a>
-            <ul className='navbar'>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About Us</a></li>
-              <li><a href="/products">Products</a></li>
-              <li><a href="/contact">Contact Us</a></li>
-            </ul>
-            <div className='main'>
-                <div className='bx bx-menu' id="menu-icon" onClick={menu}></div>
-            </div>
-        </header>
+        <h2><a href="#">Yougik</a></h2>
+        <ul className="nav">
+            <li><a href="#">home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">services</a></li>
+            <li><a href="#">contact</a></li>
+        </ul>  
+    </header>
     </div>
   )
 }
