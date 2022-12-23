e31b088aaab64de86c268f11fc8417c103832762
sthaakin04 <akinstha97@gmail.com>
2022-12-23 14:37:33 +0545

    added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,19 +11,19 @@ export default function Home() {
         <Link to="/about" className="boxBtn">More about us</Link>
       </section>
 
-      <div class="about-us">
-      <div class="text">
+      <div className="about-us">
+      <div className="text">
           <h2>Discover</h2>
           <h3>Our Story</h3>
-          <div><i class="fas fa-asterisk"></i></div>
+          <div><i className="fas fa-asterisk"></i></div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolores sit ipsam incidunt soluta aspernatur itaque pariatur! Fuga id provident quas vitae, dolore error mollitia excepturi iusto repellat, blanditiis deleniti.</p>
       </div>
-      <div class="image-container">
-          <div class="image image1">
-              <img src="https://res.cloudinary.com/abdel-rahman-ali/image/upload/v1535988527/vertical-photo-1.jpg" alt="Food Photo"/>
+      <div className="image-container">
+          <div className="image image1">
+              <img src="static/images/ag1.png" alt="aboutus photo"/>
           </div>
-          <div class="image image2">
-              <img src="https://res.cloudinary.com/abdel-rahman-ali/image/upload/v1535988532/vertical-photo-2.jpg" alt="Food Photo"/>
+          <div className="image image2">
+              <img src="static/images/ag2.png" alt="aboutus photo"/>
           </div>
       </div>
   </div>
